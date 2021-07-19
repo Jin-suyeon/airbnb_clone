9b9481739441e272a94ea09e0e13b43454687201
siyoon <younyaho@gmail.com>
2021-07-19 22:09:31 +0900

    feat:Idea, Experience, Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import CoronaNotice from './main/CoronaNotice'
+import FooterTop from './main/FooterTop';
 import Header from './main/Header'
 import MainBanner from './main/MainBanner';
 import Search from './main/Search';
 import TravelDestination from './main/TravelDestination';
+import TravelExperience from './main/TravelExperience';
+import TravelHosting from './main/TravelHosting';
 import TravelIdea from './main/TravelIdea';
 import TravelStay from './main/TravelStay';
 
@@ -19,6 +22,9 @@ function App() {
       <TravelDestination />
       <TravelStay />
       <TravelIdea />
+      <TravelExperience />
+      <TravelHosting />
+      <FooterTop />
     </div>
   );
 }
