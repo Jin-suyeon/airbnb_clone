14020d9349cdbd632e2f2c8686183a4b7e85567e
siyoon <younyaho@gmail.com>
2021-07-21 21:43:18 +0900

    feat: add hove effect in header

--- src/main/Search.js
+++ src/main/Search.js
@@ -12,43 +12,53 @@ function Search(){
         </div>
 
         <div className="search_in">
-            <span className="search_location">
+
+        {/* <div className="search_hover"> */}
+            <div className="search_location">
                 <label className="search_button">
                     <div className="search_boldtext">위치</div>
                     <input className="search_inputtext" type="text" placeholder="어디로 여행가세요?"></input>
                 </label>
-            </span>
+            </div>
+        {/* </div> */}
 
             <span className="search_bar1"></span>
-
-            <span className="search_location">
+        
+        {/* <div className="search_hover"> */}
+            <div className="search_location">
                 <label className="search_button">
                     <div className="search_boldtext">체크인</div>
                     <div className="search_inputtext">날짜 입력</div>
                 </label>
-            </span>
+            </div>
+        {/* </div> */}
 
             <span className="search_bar"></span>
 
-            <span className="search_location">
+        {/* <div className="search_hover"> */}
+            <div className="search_location">
                 <label className="search_button">
                     <div className="search_boldtext">체크아웃</div>
                     <div className="search_inputtext">날짜 입력</div>
                 </label>
-            </span>
+            </div>
+        {/* </div> */}
 
             <span className="search_bar"></span>
-
-            <span className="search_location">
+        
+        {/* <div className="search_hover"> */}
+            <div className="search_location">
                 <label className="search_button">
                     <div className="search_boldtext">인원</div>
                     <div className="search_inputtext">게스트 추가</div>
                 </label>
-            </span>
+                
+                <span className="search_findbutton">
+                    <FontAwesomeIcon className="search_findicon" icon={faSearch} />
+                </span>
+            </div>
+        {/* </div> */}
         
-            <span className="search_findbutton">
-                <FontAwesomeIcon className="search_findicon" icon={faSearch} />
-            </span>
         </div>
     </div>
   );
