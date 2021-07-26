089753c8c0cc9b6fe36a08b54fe7549d3d54ed70
siyoon <younyaho@gmail.com>
2021-07-26 21:53:45 +0900

    feat:add header usestate and hover effect

--- src/main/main_search/Search.js
+++ src/main/main_search/Search.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './Search.css'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Switch, Route} from 'react-router-dom';
 import SearchLocation from './search_sub_1/SearchLocation';
+import CheckDate from './search_sub_1/CheckDate';
 
 function Search() {
 
@@ -12,35 +13,35 @@ function Search() {
     <div className="search">
 
       <div className="search_in">
-
-        <div className="search_location">
+      
+        <Link to="/location" className="search_location">
           <label>
             <div className="search_boldtext">위치</div>
             <input className="search_inputtext" type="text" placeholder="어디로 여행가세요?"></input>
           </label>
-        </div>
+        </Link>
 
         <span className="search_bar"></span>
 
-        <div className="search_location">
+        <Link to="/checkdate" className="search_location">
           <label>
             <div className="search_boldtext">체크인</div>
             <div className="search_inputtext">날짜 입력</div>
           </label>
-        </div>
+        </Link>
 
         <span className="search_bar"></span>
 
-        <div className="search_location">
+        <Link to="/checkdate" className="search_location">
           <label>
             <div className="search_boldtext">체크아웃</div>
             <div className="search_inputtext">날짜 입력</div>
           </label>
-        </div>
+        </Link>
 
         <span className="search_bar"></span>
 
-        <div className="search_location">
+        <Link to="/personnel" className="search_location">
           <label className="search_button">
             <div className="search_boldtext">인원</div>
             <div className="search_inputtext">게스트 추가</div>
@@ -49,14 +50,24 @@ function Search() {
           <span className="search_findbutton">
             <FontAwesomeIcon className="search_findicon" icon={faSearch} />
           </span>
-        </div>
+        </Link>
 
       </div>
     </div>
 
-    <div>
-      <SearchLocation />
-    </div>
+    <Switch>
+      <Route exact path="/location">
+        <SearchLocation />
+      </Route>
+
+      <Route exact path="/checkdate">
+        <CheckDate />
+      </Route>
+
+      <Route exact path="/personnel">
+        <SearchLocation />
+      </Route>
+    </Switch>
 
     </BrowserRouter>
   );
