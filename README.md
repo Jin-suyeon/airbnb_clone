0e5c0a160008e6b6f67c481e76d611a4da7bcb51
Siyoon Jeon <jeoncue@gmail.com>
2021-10-17 19:43:20 +0900

    divided folder

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Section2부터 진행해오던 스터디입니다.<br/>
 번갈아가면서 드라이브와 네이게이터를 나누어 진행했습니다.
 
+*지금은 폴더 나누는 중
+
 #### 🛠 Stack
 HTML, CSS, React, JavaScript
 
--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
 import "./App.css";
-import CoronaNotice from "./main/CoronaNotice";
-import FooterBottom from "./main/FooterBottom";
-import FooterTop from "./main/FooterTop";
-import Header from "./main/Header";
-import MainBanner from "./main/MainBanner";
-import TravelDestination from "./main/TravelDestination";
-import TravelExperience from "./main/TravelExperience";
-import TravelHosting from "./main/TravelHosting";
-import TravelIdea from "./main/TravelIdea";
-import TravelStay from "./main/TravelStay";
+
+import { Route, Switch } from 'react-router';
+
+import Home from './pages/Home';
 
 function App() {
+
+
+  
   return (
-    <div className="App">
-      <div className="App_mainBanner_img">
-        <img
-          className="MainBanner_img"
-          src="https://a0.muscache.com/im/pictures/2da67c1c-0c61-4629-8798-1d4de1ac9291.jpg?im_w=2560"
-          alt="MainBanner"
-        />
-        <img
-          className="MainBanner_img2"
-          src="img/MainBanner.jpeg"
-          alt="MainBanner2"
-        />
-      </div>
-
-      <div className="App_main_top">
-        <CoronaNotice />
-        <Header />
-      </div>
-
-      <div className="App_mainbanner">
-        <MainBanner />
-      </div>
-
-      <div className="App_main_bottom">
-        <TravelDestination />
-        <TravelStay />
-        <TravelIdea />
-        <TravelExperience />
-        <TravelHosting />
-      </div>
-
-      <div className="App_footer">
-        <FooterTop />
-        <FooterBottom />
-      </div>
-    </div>
+    <>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+      </Switch>
+    </>
   );
 }
 
